ae98be8acc1682274036ce7d37bf29869011a4e0
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-14 21:32:08 +0530

    Add:HelloWorld

---
+++ src/com/java/datatypes/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.datatypes;
+
+// This is a public class named HelloWorld
+public class HelloWorld {
+
+    // The main method is the entry point of any Java application
+    public static void main(String[] args) {
+
+        // System.out.println is used to print text to the console
+        System.out.println("Hello World!");
+    }
+}
