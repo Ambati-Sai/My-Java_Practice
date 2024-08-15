d77ef0b4b4f23f3936ead8f43ad6226cda4348b6
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 10:06:33 +0530

    Add:float datatype

---
+++ src/com/java/datatypes/FloatDataType.java
@@ -0,0 +1,14 @@
+package com.java.datatypes;
+
+public class FloatDataType {
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+        // Create a float variable called 'height' and set it to 5.75.
+        // The 'f' at the end tells Java this number is a float, not a double.
+        float height = 5.75f;
+
+        // Print the height value to the screen.
+        // The '+' is used to add the text with the value of 'height'.
+        System.out.println("Your Height: " + height);
+    }
+}
