749ee098dcb3dfdc04bd170e204610bd15e241b1
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 10:38:02 +0530

    Operation:product of two numbers

---
+++ src/com/java/datatypes/Product.java
@@ -0,0 +1,18 @@
+package com.java.datatypes;
+
+public class Product {
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+        // Create an int variable called 'numberOne' and set it to 150.
+        int numberOne = 150;
+
+        // Create an int variable called 'numberTwo' and set it to 100.
+        int numberTwo = 100;
+
+        // Calculate the product of 'numberOne' and 'numberTwo' and store it in 'product'.
+        int product = numberOne * numberTwo;
+
+        // Print the value of 'product' to the screen.
+        System.out.println("Product of Two numbers is: " + product);
+    }
+}
