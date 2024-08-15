962c315bbef133c97dc79a9c1ad195646b3eefc1
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 10:19:49 +0530

    Add:string datatype

---
+++ src/com/java/datatypes/StringDataType.java
@@ -0,0 +1,15 @@
+package com.java.datatypes;
+
+public class StringDataType {
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+        // Create a String variable called 'studentName' and set it to "Ambati Sai".
+        // 'String' is used for text and must be enclosed in double quotes.
+        String studentName = "Ambati Sai";
+
+        // Print the value of 'studentName' to the screen.
+        // The '+' is used to combine the text with the value of 'studentName'.
+        System.out.println("My Name is: " + studentName);
+    }
+}
+
