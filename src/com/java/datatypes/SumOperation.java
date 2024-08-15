496eeb8b15cbcc18a0a756f35049bdbe57b174b5
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 10:30:22 +0530

    Operation:sum of two numbers

---
+++ src/com/java/datatypes/SumOperation.java
@@ -0,0 +1,19 @@
+package com.java.datatypes;
+
+public class SumOperation {
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+        // Create an int variable called 'firstNumber' and set it to 100.
+        int firstNumber = 100;
+
+        // Create an int variable called 'secondNumber' and set it to 80.
+        int secondNumber = 80;
+
+        // Calculate the sum of 'firstNumber' and 'secondNumber' and store it in 'sum'.
+        int sum = firstNumber + secondNumber;
+
+        // Print the value of 'sum' to the screen.
+        // The '+' is used to combine the text with the value of 'sum'.
+        System.out.println("Sum of two numbers is: " + sum);
+    }
+}
