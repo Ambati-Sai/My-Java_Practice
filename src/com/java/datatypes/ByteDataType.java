33323ddfb8453e947d5f973748081dae88e9b003
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 09:54:14 +0530

    Add:byte datatype

---
+++ src/com/java/datatypes/ByteDataType.java
@@ -0,0 +1,13 @@
+package com.java.datatypes;
+
+public class ByteDataType {
+    // The main method is the entry point for the Java application.
+    public static void main(String[] args) {
+        // Declare a byte variable named 'orderNumber' and initialize it with the value 127.
+        byte orderNumber = 127;
+
+        // Print out the value of 'orderNumber' to the console.
+        // The '+' operator is used to concatenate the string with the value of 'orderNumber'.
+        System.out.println("Your orderNumber is: " + orderNumber);
+    }
+}
