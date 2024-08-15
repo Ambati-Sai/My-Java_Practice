853a1eecbc8064998ae50ea50a37f1d8b7698254
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 09:57:53 +0530

    Add:short datatype

---
+++ src/com/java/datatypes/ShortDataType.java
@@ -0,0 +1,13 @@
+package com.java.datatypes;
+
+public class ShortDataType {
+    // The main method is the entry point for the Java application.
+    public static void main(String[] args) {
+        // Declare a short variable named 'employeeSalary' and initialize it with the value 30000.
+        short employeeSalary = 30000;
+
+        // Print out the value of 'employeeSalary' to the console.
+        // The '+' operator is used to concatenate the string with the value of 'employeeSalary'.
+        System.out.println("Employee Salary: " + employeeSalary);
+    }
+}
