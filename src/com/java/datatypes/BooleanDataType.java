842a3d12413c1ab3ea9cd068b3c3f8cf63982914
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 10:16:29 +0530

    Add:boolean datatype

---
+++ src/com/java/datatypes/BooleanDataType.java
@@ -0,0 +1,21 @@
+package com.java.datatypes;
+
+public class BooleanDataType {
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+        // Create a boolean variable called 'ifYes' and set it to true.
+        // 'boolean' is used for true or false values.
+        boolean ifYes = true;
+
+        // Create a boolean variable called 'ifNo' and set it to false.
+        boolean ifNo = false;
+
+        // Print the value of 'ifYes' to the screen.
+        // The '+' is used to combine the text with the value of 'ifYes'.
+        System.out.println("Your name is correct: " + ifYes);
+
+        // Print the value of 'ifNo' to the screen.
+        // The '+' is used to combine the text with the value of 'ifNo'.
+        System.out.println("Your name is incorrect: " + ifNo);
+    }
+}
