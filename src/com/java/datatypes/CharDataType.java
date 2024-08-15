d6aebbc34ef3a618d5b4cc4ed2a88306a45f962e
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 10:14:05 +0530

    Add:char datatype

---
+++ src/com/java/datatypes/CharDataType.java
@@ -0,0 +1,14 @@
+package com.java.datatypes;
+
+public class CharDataType {
+    // This is the main method where the program starts running.
+    public static void main(String[] args) {
+
+        // 'char' is used for single characters and must be enclosed in single quotes.
+        char firstAlphabet = 'A';
+
+        // Print the value of 'firstAlphabet' to the screen.
+        // The '+' is used to add the text with the value of 'firstAlphabet'.
+        System.out.println("First Alphabet Letter is: " + firstAlphabet);
+    }
+}
