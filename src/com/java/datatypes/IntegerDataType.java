d23ff1b97666f0750b791cef39c28a933524afe2
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-15 09:59:40 +0530

    Add:integer datatype

---
+++ src/com/java/datatypes/IntegerDataType.java
@@ -0,0 +1,11 @@
+package com.java.datatypes;
+
+public class IntegerDataType {
+    public static void main(String[] args) {
+        // Declare and initialize an int variable
+        int rollNumber = 402;
+        // Print the value of the variable
+        System.out.println( "Student rollNumber :" + rollNumber);
+    }
+
+}
