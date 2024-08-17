1b70ae85b7ae3470a19993916514b7a0115c3e7f
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-17 09:51:56 +0530

    add:Ternary operator

---
+++ src/com/java/operators/TernaryOperators.java
@@ -0,0 +1,14 @@
+package com.java.operators;
+
+public class TernaryOperators {
+    public static void main(String[] args) {
+        int number = 7;
+
+            // Use the ternary operator to check if the number is even or odd
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+        
+        System.out.println("The number " + number + " is " + result + ".");
+        }
+    }
+
+
