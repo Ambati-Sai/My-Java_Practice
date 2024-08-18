9a2eafd9a61cb6e30785921eac0fa76c24aeeea6
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-18 14:15:06 +0530

    Add:else example

---
+++ src/com/java/ifelse/ElseExample.java
@@ -0,0 +1,19 @@
+package com.java.ifelse;
+
+public class ElseExample {
+    public static void main(String[] args) {
+
+        int numberOne = 100;
+
+        int numberTwo = 200;
+
+        // Check if numberOne is greater than numberTwo
+        if (numberOne > numberTwo) {
+            // Print a message if numberOne is greater than numberTwo
+            System.out.println("Number one is greater than Number Two");
+        } else {
+            // Print a message if numberOne is not greater than numberTwo
+            System.out.println("NumberOne is less than Number Two");
+        }
+    }
+}
