f900a189c74102925707f7ba9fc35296ef9fb59f
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-18 14:44:52 +0530

    Add :switch example

---
+++ src/com/java/ifelse/SwitchExample.java
@@ -0,0 +1,47 @@
+package com.java.ifelse;
+
+import java.util.Scanner;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number (1-7) to get the corresponding day of the week: ");
+
+        // Read the user input
+        int weekDay = scanner.nextInt();
+
+        // Use a switch statement to determine the day of the week
+        switch (weekDay) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                // Handle invalid input
+                System.out.println("Incorrect input. Please enter a number between 1 and 7.");
+                break;
+        }
+
+        // Close the scanner to prevent resource leaks
+        scanner.close();
+    }
+}
