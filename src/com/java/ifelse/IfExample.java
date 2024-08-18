e4d1ec180974878d2517fba276abaec6e41b7b09
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-18 13:54:28 +0530

    add:if Example

---
+++ src/com/java/ifelse/IfExample.java
@@ -0,0 +1,16 @@
+package com.java.ifelse;
+
+public class IfExample {
+    public static void main(String[] args) {
+
+        int numberOne = 150;
+
+        int numberTWo = 200;
+
+        // Check if numberOne is less than numberTWo
+        if (numberOne < numberTWo) {
+            // Print a message if the condition is true
+            System.out.println("NumberOne is less than NumberTWo");
+        }
+    }
+}
