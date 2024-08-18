d1c7beebf728f930538c53422e0671a6378ac84d
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-18 15:01:38 +0530

    Add:do-while exanple

---
+++ src/com/java/ifelse/DoWhileExample.java
@@ -0,0 +1,19 @@
+package com.java.ifelse;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        // Start of the do-while loop
+        do {
+            // Print the current value of number
+            System.out.println(number);
+
+            // Increment the value of number by 1
+            number++;
+
+            // Condition to check after the loop body
+        } while (number < 10);
+    }
+}
