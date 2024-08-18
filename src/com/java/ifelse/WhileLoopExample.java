449ba93ec66768386a8289f3e333e2ce54d74c98
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-18 14:55:31 +0530

    Add :while loop example

---
+++ src/com/java/ifelse/WhileLoopExample.java
@@ -0,0 +1,17 @@
+package com.java.ifelse;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+
+        int number = 0;
+
+        // Continue looping as long as number is less than 10
+        while (number < 10) {
+            // Print the current value of number
+            System.out.println(number);
+
+            number++;
+        }
+    }
+}
+
