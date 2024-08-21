7f1dbe189f35bc2ad129f8be252dc3a05e341162
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-21 17:52:06 +0530

    Add:for loop exanple

---
+++ src/com/java/ifelse/ForLoopExample.java
@@ -0,0 +1,25 @@
+package com.java.ifelse;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Declare and initialize the variable numberOne
+        int numberOne = 10;
+
+        // Initialize numberOne to 0
+        // Continue looping while numberOne is less than or equal to 10
+        // Increment numberOne by 1 after each iteration
+        for (numberOne = 0; numberOne <= 10; numberOne++) {
+            // Print the current value of numberOne
+            System.out.println(numberOne);
+        }
+
+        // Reinitialize numberOne to 0
+        // Continue looping while numberOne is less than or equal to 10
+        // Increment numberOne by 2 after each iteration
+        for (numberOne = 0; numberOne <= 10; numberOne = numberOne + 2) {
+            // Print the current value of numberOne
+            System.out.println(numberOne);
+        }
+    }
+}
+
