03ac25de2ad5051a51b8eb984448818b5281d596
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-21 18:10:21 +0530

    Add:break exanple

---
+++ src/com/java/ifelse/BreakExample.java
@@ -0,0 +1,21 @@
+package com.java.ifelse;
+
+public class BreakExample {
+    public static void main(String[] args) {
+        // Declare and initialize the variable num
+        int num = 10;
+
+        // Start a for loop with num initialized to 0
+        // Loop continues while num is less than 10
+        // Increment num by 1 after each iteration
+        for (num = 0; num < 10; num++) {
+            // Check if num is equal to 5
+            if (num == 5) {
+                // Exit the loop if num is equal to 5
+                break;
+            }
+            // Print the current value of num
+            System.out.println(num);
+        }
+    }
+}
