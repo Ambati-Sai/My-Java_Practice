1ea72734e14a78cb991c34e87b33fe47745e9591
Ambati-Sai <22r95a7402@tkrec.ac.in>
2024-08-23 13:31:22 +0530

    Add:number pattern

---
+++ src/com/java/patterns/NumberPattern.java
@@ -0,0 +1,13 @@
+package com.java.patterns;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        int num = 10;
+        for (int line = 1; line <= num; line++) {
+            for (int number = 1; number <= line; number++) {
+                System.out.print(number);
+            }
+            System.out.println(); // Moves to the next line after printing stars
+        }
+    }
+}
